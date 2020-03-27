1ee4d079b51d526072580a62e48e949ce4984512
Kamil <kamil.lesniak@afiszmedia.pl>
2020-03-27 23:20:20 +0100

    Remove bootstrap js file

--- public/js/http/bootstrap.js
+++
@@ -1,6 +0,0 @@
-let menuToggler = '';
-if(menuToggler = document.querySelector('button[data-target="#navbarNav"]')) {
-  menuToggler.onclick = () => {
-    document.getElementById('navbarNav').classList.toggle('d-flex');
-  };
-}
