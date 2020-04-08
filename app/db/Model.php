e637bb05881ea48061a7a144ba44e993c9def720
Kamil <kamil.lesniak@afiszmedia.pl>
2020-04-08 22:59:18 +0200

    Rename accessor to model, now default model can connet do database, using function table to set table and exe queries

--- app/db/Model.php
+++ app/db/Model.php
@@ -2,11 +2,19 @@
 
 namespace App\Db;
 
-class Accessor
+class Model
 {
+    public static $table;
+
     public static function __callStatic($name, $arguments)
     {
         $model = new Db(get_called_class());
         return $model->$name($arguments[0] ?? $arguments);
+    }
+
+    public static function table($table)
+    {
+        self::$table = $table;
+        return new Db(get_called_class());
     }
 }
