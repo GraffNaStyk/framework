04011323dee652b4c344e6613e49d2a64674e651
Kamil <kamil.lesniak@afiszmedia.pl>
2020-04-16 21:33:25 +0200

    changes await for app.js

--- public/js/app.js
+++ public/js/app.js
@@ -29,59 +29,56 @@ export const get = async (fetch_url) => {
   }).then(res => res.json());
 };
 
-
-export const render = async (args) => {
-  fetch(url + args.url, {
+export const render = (args) => {
+  return fetch(url + args.url, {
     method: 'GET',
     credentials: 'same-origin',
     headers: {
       "X-Fetch-Header": "fetchApi",
     },
-  }).then((res) => res.text())
-  .then(result => {
-    let script = '';
-    if(args.src) {
-      script = document.createElement('script');
-      script.setAttribute('type', 'module');
-      script.setAttribute('src',`public/js/${args.src}`);
-    }
-
+  }).then(async (res) => res.text())
+  .then(async result => {
     if (args.el !== 'modal') {
       if(args.append)
         document.querySelector(`[data-component="${args.el}"]`).innerHTML += result;
       else {
-        document.querySelector(`[data-component="${args.el}"]`).appendChild(script);
         document.querySelector(`[data-component="${args.el}"]`).innerHTML = result;
       }
     } else {
-      document.querySelector(`#modal`).appendChild(script);
       modal(result);
     }
-  })
-};
-
-export const view = async (name) => {
-  fetch(url + `Dash/view/${name}`, {
-    method: 'GET',
-    credentials: 'same-origin',
-    headers: {
-      "X-Fetch-Header": "fetchApi",
-    },
-  }).then(res => res.text())
-  .then(result => {
-    console.log(result);
   })
 };
 
 const modal = (result) => {
   const modal = document.getElementById('modal');
   modal.classList.add('d-block');
-  modal.setAttribute('style', 'background: rgba(0,0,0,0.8)');
-  modal.innerHTML = result;
+  modal.setAttribute('style', 'background: rgba(0,0,0,0.3)');
+  modal.innerHTML += result;
 
   document.querySelector('button[data-dismiss="modal"]').onclick = () => {
     modal.classList.remove('d-block');
     modal.innerHTML = '';
     modal.setAttribute('style', '');
   };
+};
+
+export const response = (res, selector) => {
+  let min = Math.ceil(500);
+  let max = Math.floor(150000);
+  let rand = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  document.querySelector(`${selector}`).insertAdjacentHTML('beforeBegin', `
+      <div data-${rand}="" class="alert alert-${res.class}" role="alert">
+          ${res.msg}
+        </div>
+     `);
+
+  let alert = document.querySelector(`[data-${rand}=""]`);
+  if(alert) {
+    setTimeout(() => {
+      alert.remove();
+    }, 3000)
+  }
+
 };
