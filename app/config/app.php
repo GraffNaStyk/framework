f5db4b4134630c111f04db97779a40daa29721fe
Kamil <kamil.lesniak@afiszmedia.pl>
2020-04-27 16:46:19 +0200

    Remvoe unused variables

--- app/config/app.php
+++ app/config/app.php
@@ -2,46 +2,24 @@
 
 return [
     /**
-     * @csrf is used to blocking csrf attack from users,
-     * if this variable is set to true, you need to add for very form
-     * twig variable like {{ csrf() }}, csrf is not checked if your
-     * request is pushed by js Api fetch method !
+     *  @csrf is used to blocking csrf attack from users,
+     *  if this variable is set to true, you need to add for very form
+     *  twig variable like {{ csrf() }}, csrf is not checked if your
+     *  request is pushed by js Api fetch method !
      */
     'csrf' => false,
 
     /*
-     * @dev here tou can set developer mode to true or false, if developer mode
-     * is set to true on page have all bugs
+     *  @dev here tou can set developer mode to true or false, if developer mode
+     *  is set to true on page have all bugs
      */
-    'dev' => false,
-
-    /*
-     * @admin if variable admin is set to true, application automatic redirect to
-     * admin panel, page is offline
-     */
-    'admin' => false,
+    'dev' => true,
 
     /*
      *  @url this is a framework url, default u can set '/' if framework exist
-     * in any sub folder need to add this path there to good working
+     *  in any sub folder need to add this path there to good working
     */
     'url' => '/graff/',
-
-    /*
-    * @cms_url is a calling url for go to management system for page, default is a admin like
-     *  www.page.pl/admin you can write here every link
-    */
-    'cms' => 'cms',
-
-    /*
-     *   @controller Set default controller for framework, current default controller is every item Index
-     */
-    'controller' => 'index',
-
-    /*
-     * @action Set default action for framework, current default action is every item Index
-     */
-    'action' => 'index',
 
     /*
      *  @mail configuration using in framework to send mails.
