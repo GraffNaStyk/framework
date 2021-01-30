d0a84027f1c57e579e4d3f307c8090ad31f6dc20
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-01-30 16:30:57 +0100

    clear code

--- app/config/app.php
+++ app/config/app.php
@@ -20,7 +20,7 @@ return [
      *  @url this is a framework url, default u can set '/' if framework exist
      *  in any sub folder need to add this path there to good working
     */
-    'url' => '/graff',
+    'url' => '/',
     
     /*
      *  @cache_view disable or enable view caching
