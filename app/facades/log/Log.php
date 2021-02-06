36c171398150b9fd6c298c2309a53f6b57fe4b8e
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-02-06 23:28:30 +0100

    clear code

--- app/facades/log/Log.php
+++ app/facades/log/Log.php
@@ -8,11 +8,6 @@ class Log
 {
     private static function make(string $type, array $data): void
     {
-        $data['trace'] = [
-            'controller' => Router::getNamespace() . '\\' . Router::getClass(),
-            'action' => Router::getAction()
-        ];
-    
         $data['client'] = php_sapi_name();
         $data['date'] = date('Y-m-d H:i:s');
     
@@ -26,10 +21,9 @@ class Log
                 json_encode($content, JSON_PRETTY_PRINT)
             );
         } else {
-            $data = array_reverse($data);
             file_put_contents(
                 storage_path('private/logs/' . $type . '_' . date('Y-m-d') . '.json'),
-                json_encode([$data], JSON_PRETTY_PRINT)
+                json_encode([array_reverse($data)], JSON_PRETTY_PRINT)
             );
         }
     }
