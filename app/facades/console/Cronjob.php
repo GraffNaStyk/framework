3f0b23a5738b7daa963ecec677c4e0d44dbcbb8b
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-02-06 13:07:39 +0100

    Add cronjob to console, Add readme.md

--- app/facades/console/Cronjob.php
+++ app/facades/console/Cronjob.php
@@ -4,5 +4,33 @@ namespace App\Facades\Console;
 
 class Cronjob
 {
-
+    protected string $path = '\\App\\Cronjobs\\';
+    private string $name;
+    protected string $file;
+    
+    public function __construct($args = [])
+    {
+        $this->name = $args[0];
+        $this->file = file_get_contents(app_path('app/facades/http/cronjob'));
+    }
+    
+    public function make()
+    {
+        $this->file = str_replace('CLASSNAME', ucfirst($this->name).'Cronjob', $this->file);
+        if (file_put_contents(
+            app_path('app/cronjobs/'.ucfirst($this->name).'Cronjob.php'),
+            $this->file
+        )) {
+            Console::output(
+                'Cronjob in path: app/cronjobs/'.ucfirst($this->name).'Cronjob.php created.',
+                'green'
+            );
+        }
+    }
+    
+    public function run()
+    {
+        $class = $this->path.$this->name.'Cronjob';
+        (new $class());
+    }
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+##Console
+| Typ | Opis |
+| ------ | ------ |
+| Crontab | time php path/to/console cronjob method - run,make fileName |
+| Controller | php console controller method - run,make fileName |
+| Model | php console model make fileName Table |
+| Migrate | php console migrate method - up,down,make fileName table |
+| Middleware | php console middleware make fileName |
+| Rule | php console rule make fileName |
