7f362e85c43dc41d7b8051e2fce1e3804ce691c0
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-02-07 20:31:47 +0100

    optimize autoloader code

--- app/facades/autoload/Autoload.php
+++ app/facades/autoload/Autoload.php
@@ -6,25 +6,10 @@ class Autoload
 {
     public static function run(string $class): void
     {
-        $classArr = explode('\\', $class);
-        $className = end($classArr);
-    
-        array_pop($classArr);
-        $classArr = array_map('strtolower', $classArr);
-        $path = '';
-    
-        foreach ($classArr as $namespaces) {
-            $path .= $namespaces.'/';
-        }
-    
-        $className = rtrim($className, '/');
-    
-        if ((bool) file_exists(path($path . $className .'.php')) === true) {
-            require_once path($path . $className .'.php');
-        }
-    
-        if ((bool) file_exists(path($path . $className .'.inc')) === true) {
-            require_once path($path . $className .'.inc');
+        $class = mb_strtolower(str_replace('\\', '/', $class)).'.php';
+        
+        if ((bool) file_exists(path($class))) {
+            require_once path($class);
         }
     }
 }
