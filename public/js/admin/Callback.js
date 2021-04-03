bb45dc49abfb19772fe9f4e505eab48c1dfff2fc
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-04-03 13:29:48 +0200

    code clear callback js

--- public/js/admin/Callback.js
+++ public/js/admin/Callback.js
@@ -1,16 +1,14 @@
 
 const callback = (res) => {
-  if (res.ok) {
-    if (res.reload) {
-      if (res.to !== null && res.to !== '') {
-        setTimeout(() => {
-          document.location.href = $.url + res.to;
-        }, 700);
-      } else {
-        setTimeout(() => {
-          document.location.reload();
-        }, 700);
-      }
+  if (res.ok && res.reload) {
+    if (res.to !== null && res.to !== '') {
+      setTimeout(() => {
+        document.location.href = $.url + res.to;
+      }, 700);
+    } else {
+      setTimeout(() => {
+        document.location.reload();
+      }, 700);
     }
   }
 };
