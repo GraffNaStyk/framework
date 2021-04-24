63a1ff028237ed99e59eefcf4437377190d895f4
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-04-24 11:42:01 +0200

    reformat migration code

--- app/db/migrate/Migration_Right_2020_09_24__22_26.php
+++ app/db/migrate/Migration_Right_2020_09_24__22_26.php
@@ -1,21 +1,22 @@
 <?php
+
 namespace App\Db\Migrate;
 
 use App\Facades\Migrations\Schema;
 
 class Migration_Right_2020_09_24__22_26
 {
-       public string $model = 'Right';
-
-       public function up(Schema $schema)
-       {
-           $schema->int('id', 11)->primary();
-           $schema->int('user_id', 11)->unique();
-           $schema->run();
-       }
-
-       public function down(Schema $schema)
-       {
-           $schema->clear();
-       }
+	public string $model = 'Right';
+	
+	public function up(Schema $schema)
+	{
+		$schema->int('id', 11)->primary();
+		$schema->int('user_id', 11)->unique();
+		$schema->run();
+	}
+	
+	public function down(Schema $schema)
+	{
+		$schema->clear();
+	}
 }
