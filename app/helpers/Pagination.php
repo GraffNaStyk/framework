f3c5891d1c0382a48f0786785d1c4f66d5c29ce3
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-05-16 12:27:43 +0200

    Add pagination component

--- app/helpers/Pagination.php
+++ app/helpers/Pagination.php
@@ -3,17 +3,28 @@
 namespace App\Helpers;
 
 use App\Core\App;
+use App\Facades\Http\Router\Router;
 use App\Facades\Http\View;
+use App\Facades\Url\Url;
 
 class Pagination
 {
     public static function make(string $model, int $page, string $url)
     {
+    	$alias = Url::full().'/'.Router::getAlias();
+		$total = ceil($model::count('id')->get()->count / App::PER_PAGE);
+		
         View::set([
             'pagination' => [
-                'previous' => $url.($page-1),
-                'next'     => $url.($page+1),
-                'total'    => ceil($model::count('id')->get()->total / App::PER_PAGE)
+                'previousLink' => $alias.$url.'/'.($page-1),
+                'nextLink'     => $alias.$url.'/'.($page+1),
+                'previous'     => ($page-1),
+                'next'         => ($page+1),
+                'currentPage'  => ($page),
+                'total'        => $total,
+	            'first'        => $alias.$url,
+	            'last'         => $alias.$url.'/'.$total,
+	            'current'      => $alias.$url.'/'.($page)
             ]
         ]);
     }
