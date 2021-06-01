d7f042153c31200c9f14ebe562db923633f629f3
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-06-01 18:23:21 +0200

    fix typo issue

--- app/config/app_example.php
+++ app/config/app_example.php
@@ -4,8 +4,8 @@ return [
 	
     /**
      *  @csrf is used to blocking csrf attack from users,
-     *  if this variable is set to true, you need to add for very form
-     *  twig variable like {{ form.csrf('Controller@acion') }}
+     *  if this variable is set to true, you need to add for every form
+     *  twig variable like {{ form.csrf('Controller@action') }}
      */
     'csrf' => true,
 
