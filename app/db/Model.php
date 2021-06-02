213b91d68f215a6baa792461d12a0a48b3d0b7d3
kamil.lesniak94@gmail.com <kamil.lesniak@salesbook-app.com>
2021-06-02 14:29:38 +0200

    Short code in Model.php

--- app/db/Model.php
+++ app/db/Model.php
@@ -4,16 +4,13 @@ namespace App\Db;
 
 class Model
 {
-    private static ?object $db = null;
 
     public static function __callStatic(string $name, array $arguments)
     {
-	    self::$db = new Db(get_called_class());
-	
 	    if (isset($arguments[1])) {
-		    return call_user_func_array([self::$db, $name], $arguments);
+		    return call_user_func_array([new Db(get_called_class()), $name], $arguments);
 	    }
 	
-	    return self::$db->$name($arguments[0] ?? $arguments);
+	    return (new Db(get_called_class()))->$name($arguments[0] ?? $arguments);
     }
 }
