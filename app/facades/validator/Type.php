6c38b253abee1e9fa6ac346b35499f01734dbf1f
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-06-25 16:08:19 +0200

    Fix for max int value

--- app/facades/validator/Type.php
+++ app/facades/validator/Type.php
@@ -19,6 +19,10 @@ class Type
         } else if (preg_match('/^[+-]?(\d*\.\d+([eE]?[+-]?\d+)?|\d+[eE][+-]?\d+)$/', str_replace([',', ' '], ['.', ''], $item))) {
             return (float) str_replace([',', ' '], ['.', ''], $item);
         } else if (is_numeric($item)) {
+            if ((int) $item >= 2147483647) {
+	            return 2147483647;
+	        }
+
             return (int) $item;
         } else {
             return (string) trim($item);
