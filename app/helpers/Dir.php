7b260a314cb07598217f2f21e1f88c62b8983f8a
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-07-05 22:22:23 +0200

    fix spaces

--- app/helpers/Dir.php
+++ app/helpers/Dir.php
@@ -5,10 +5,10 @@ namespace App\Helpers;
 
 class Dir
 {
-	public static function create(string $path): void
-	{
-		if (! is_dir($path)) {
-			mkdir($path, 0775, true);
-		}
-	}
+    public static function create(string $path): void
+    {
+        if (! is_dir($path)) {
+            mkdir($path, 0775, true);
+        }
+    }
 }
