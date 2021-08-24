115e883a6c6f693c42e8239c6570744e50cfe513
GraffNaStyk <kamil.lesniak94@gmail.com>
2021-08-24 22:10:41 +0200

    Model code refactor

--- app/facades/db/Model.php
+++ app/facades/db/Model.php
@@ -4,13 +4,13 @@ namespace App\Facades\Db;
 
 class Model
 {
-
-    public static function __callStatic(string $name, array $arguments)
-    {
-        if (isset($arguments[1])) {
-            return call_user_func_array([new Db(get_called_class()), $name], $arguments);
-        }
-
-        return (new Db(get_called_class()))->$name($arguments[0] ?? $arguments);
-    }
+	
+	public static function __callStatic(string $name, array $arguments)
+	{
+		if (isset($arguments[1])) {
+			return call_user_func_array([new Db(get_called_class()), $name], $arguments);
+		}
+		
+		return (new Db(get_called_class()))->$name($arguments[0] ?? $arguments);
+	}
 }
