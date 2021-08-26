ac630d29111618aa4fd644ece433c8a57d4cdbfb
kamil.lesniak@salesbook-app.com <kamil.lesniak94@gmail.com>
2021-08-26 22:15:10 +0200

    format code

--- app/core/App.php
+++ app/core/App.php
@@ -8,16 +8,16 @@ use App\Helpers\Loader;
 
 final class App
 {
-    const PER_PAGE = 25;
-
-    public function run(): void
-    {
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
-
-        Env::set();
-	    Db::init();
-        Loader::set();
-    }
+	const PER_PAGE = 25;
+	
+	public function run(): void
+	{
+		if (session_status() === PHP_SESSION_NONE) {
+			session_start();
+		}
+		
+		Env::set();
+		Db::init();
+		Loader::set();
+	}
 }
