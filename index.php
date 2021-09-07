722a4ed9b052441f7f6406ef739537611d3eafc0
kamil.lesniak@salesbook-app.com <kamil.lesniak94@gmail.com>
2021-09-07 22:01:38 +0200

    add parameter types in index.php functions

--- index.php
+++ index.php
@@ -14,42 +14,42 @@ if (! is_readable(vendor_path('/autoload.php'))) {
 	exit(require_once view_path('/errors/install.php'));
 }
 
-function css_path($path = null): string
+function css_path(?string $path = null): string
 {
 	return __DIR__ . '/public/css/' . ltrim($path, '/');
 }
 
-function js_path($path = null): string
+function js_path(?string $path = null): string
 {
 	return __DIR__ . '/public/js/' . ltrim($path, '/');
 }
 
-function view_path($path = null): string
+function view_path(?string $path = null): string
 {
 	return __DIR__ . '/app/views/' . ltrim($path, '/');
 }
 
-function app_path($path = null): string
+function app_path(?string $path = null): string
 {
 	return __DIR__ . '/' . ltrim($path, '/');
 }
 
-function storage_path($path = null): string
+function storage_path(?string $path = null): string
 {
 	return __DIR__ . '/storage/' . ltrim($path, '/');
 }
 
-function assets_path($path = null): string
+function assets_path(?string $path = null): string
 {
 	return __DIR__ . '/public/assets/' . ltrim($path, '/');
 }
 
-function vendor_path($path = null): string
+function vendor_path(?string $path = null): string
 {
 	return __DIR__ . '/vendor/' . ltrim($path, '/');
 }
 
-function path($path = null): string
+function path(?string $path = null): string
 {
 	return __DIR__ . '/' . ltrim($path, '/');
 }
