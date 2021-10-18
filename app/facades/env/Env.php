9bcfd81d6efe9229c93dc5b5c8018ed31fa15e78
kamil.lesniak@salesbook-app.com <kamil.lesniak94@gmail.com>
2021-10-18 19:35:57 +0200

    Fix Env, remove new lines tabs etc

--- app/facades/env/Env.php
+++ app/facades/env/Env.php
@@ -14,11 +14,17 @@ class Env
     		return;
 	    }
     	
-        $env = file_get_contents(app_path('app/config/.env'));
+        $env = file_get_contents(app_path('/app/config/.env'));
         $environment = [];
 
         foreach (array_filter(explode(PHP_EOL, $env)) as $item) {
+	        $item = str_replace([' ', "\n", "\t", "\r"], '', $item);
             $item = explode('=', $item);
+
+            if ($item[0] === '') {
+            	continue;
+            }
+
             $environment[trim($item[0])] = trim($item[1]);
         }
 
