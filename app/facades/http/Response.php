d136e6fb0d6f9fd29de0e77506118dcc997fb53c
kamil.lesniak@salesbook-app.com <kamil.lesniak94@gmail.com>
2021-11-04 18:19:35 +0100

    add response object

---
+++ app/facades/http/Response.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Facades\Http;
+
+use App\Facades\Header\Header;
+
+class Response
+{
+	use Header;
+	
+	const RESPONSE_CODES = [
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		500 => 'Internal Server Error'
+	];
+	
+	private array $data = [];
+	
+	private ?string $content = null;
+	
+	private bool $isJsonResponse = false;
+	
+	private array $customHeaders = [];
+	
+	private int $responseCode = 200;
+	
+	public function __construct()
+	{
+		return $this;
+	}
+	
+	public function setHeader(string $name, string $value): self
+	{
+		$this->customHeaders[$name] = $value;
+		
+		return $this;
+	}
+	
+	public function setHeaders(array $headers): self
+	{
+		foreach ($headers as $key => $header) {
+			$this->setHeader($key, $header);
+		}
+		
+		return $this;
+	}
+	
+	public function json(): self
+	{
+		$this->isJsonResponse = true;
+		
+		return $this;
+	}
+	
+	public function setData(array $data): self
+	{
+		$this->data	= $data;
+		
+		return $this;
+	}
+	
+	public function setContent(string $content): self
+	{
+		$this->content = $content;
+		
+		return $this;
+	}
+	
+	public function send(): self
+	{
+		return $this;
+	}
+	
+	public function setCode(int $code = 200): self
+	{
+		$this->responseCode = $code;
+		
+		return $this;
+	}
+	
+	public function getResponse(): string
+	{
+		$this->prepareHeaders();
+		http_response_code($this->responseCode);
+		
+		if (! empty($this->customHeaders)) {
+			foreach ($this->customHeaders as $name => $value) {
+				\header($name.': '.$value);
+			}
+		}
+		
+		if ($this->content !== null) {
+			return $this->content;
+		} else if ($this->isJsonResponse) {
+			return json_encode($this->data);
+		}
+		
+		throw new \LogicException('Wrong response send! Set response content or response data');
+	}
+}
