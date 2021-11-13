9d57d8702b995240a893acdcec71bb6f841a2139
kamil.lesniak@salesbook-app.com <kamil.lesniak94@gmail.com>
2021-11-13 13:37:19 +0100

    Refactor faker, faster work, less code

--- app/facades/faker/Faker.php
+++ app/facades/faker/Faker.php
@@ -4,51 +4,10 @@ namespace App\Facades\Faker;
 
 class Faker
 {
-    private static array $letters = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-        'r', 's', 't', 'u', 'w', 'x', 'y', 'z'
-    ];
-
-    private static array $numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    private static array $methods = ['int', 'string'];
-
-    public static function string(int $length = 0): string
-    {
-        $string = null;
-        $count = count(static::$letters) - 1;
-
-        for ($i = 0; $i < $length; $i ++) {
-            $string .= static::$letters[rand(0, $count)];
-        }
-
-        return $string;
-    }
-
-    public static function int(int $length = 0): int
-    {
-        $number = null;
-        $count  = count(static::$numbers) - 1;
-
-        for ($i = 0; $i < $length; $i ++) {
-            $number .= static::$numbers[rand(0, $count)];
-        }
-
-        return $number;
-    }
-
-    public static function hash(int $length): string
-    {
-        $password = '';
-		$strLen   = strlen($password);
-		
-        while ($strLen < $length) {
-            $method    = self::$methods[rand(0, 1)];
-            $password .= self::$method(1);
-        }
-
-        return $password;
-    }
+	public static function hash(int $length): string
+	{
+		return bin2hex(random_bytes($length));
+	}
 
     public static function getUniqueStr(string $model, string $column = 'hash', int $length = 50): string
     {
